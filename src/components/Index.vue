4e6e5db7da43d56e4fb93c78235301295ed8fbcb
nbhaohao <nbhaohao@163.com>
2021-02-12 22:02:36 +0800

    feat:初始化项目

---
+++ src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
